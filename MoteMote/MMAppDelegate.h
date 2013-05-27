38d13832b8595f7392689076d8dfff38e3cc5d4e
Cameron Ehrlich <cameronehrlich@gmail.com>
2013-05-26 20:57:25 -0700

    Initial Commit

---
+++ MoteMote/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  MoteMote
+//
+//  Created by Cameron Ehrlich on 5/26/13.
+//  Copyright (c) 2013 Cameron Ehrlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MoteMote/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  MoteMote
+//
+//  Created by Cameron Ehrlich on 5/26/13.
+//  Copyright (c) 2013 Cameron Ehrlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
